72d98d9f4275d03e09db30a068e068f9868932cb
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-07-12 10:18:45 -0600

    update seed-database.js util to insert folder data

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,15 +4,25 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
 const seedNotes = require('../db/seed/notes');
+const seedFolders = require('../db/seed/folders');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(seedNotes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes(),
+    ]); 
+  })
   .then(results => {
     // eslint-disable-next-line no-console
-    console.info(`Inserted ${results.length} Notes`);
+    console.info(`Inserted ${results[0].length} Notes`);
+    console.info(`Inserted ${results[1].length} Folders`);
+    console.info('Created Indexes on Folders');
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
