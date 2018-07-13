105025e3033c54c41df955e584179b7d936533f2
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-07-13 12:55:19 -0600

    add CRUD endpoints for tags

--- routes/tags.js
+++ routes/tags.js
@@ -4,18 +4,137 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Note = require('../models/note');
-const Folder = require('../models/folder');
 const Tag = require('../models/tag');
 
-const router = express.Router;
+const router = express.Router();
 
-/* ========== GET/READ ALL TAGS ========== */
+/* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  Tag.find()
+    .collation({ locale: 'en' })
+    .sort({ name: 'asc' })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Tag.findById(id)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== POST/CREATE A SINGLE ITEM ========== */
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = (400);
+    return next(err);
+  }
+
+  const newTag = {
+    name
+  };
+
+  Tag.create(newTag)
+    .then(response => {
+      res.location(`${req.headers.orignalUrl}/${response.body.id}`)
+        .status(201)
+        .json(response);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The tag name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
 
 });
 
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
 
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
 
+  const updateObj = { $set: { name } };
+  const options = { new: true };
+
+  Tag.findByIdAndUpdate(id, updateObj, options)
+    .then(updatedTag => {
+      if (updatedTag) {
+        res.json(updatedTag);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The tag name already exists');
+        err.status = (400);
+      }
+      next(err);
+    });
+});
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  const tagDeletePromise = Tag.findByIdAndRemove(id);
+  const noteUpdatePromise = Note.updateMany(
+    { tags: id },
+    { $pull: { tags: id } }
+  );
+
+  Promise.all([
+    tagDeletePromise,
+    noteUpdatePromise
+  ])
+    .then(response => {
+      if (response) {
+        res.json(response);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
